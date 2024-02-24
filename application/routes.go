1db7773c0f95cf8db15dd344fb76ee19123a8695
Sarvesh Hon <sarveshhon@gmail.com>
2024-02-25 04:37:20 +0530

    refactored code and routes

--- application/routes.go
+++ application/routes.go
@@ -2,6 +2,7 @@ package application
 
 import (
 	"net/http"
+	"restmanage/handler"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
@@ -15,5 +16,17 @@ func loadRoutes() *chi.Mux {
 		w.WriteHeader(http.StatusOK)
 	})
 
+	router.Route("/orders", loadOrderRoutes)
+
 	return router
+}
+
+func loadOrderRoutes(router chi.Router) {
+	orderHandler := &handler.Order{}
+
+	router.Post("/", orderHandler.Create)
+	router.Get("/", orderHandler.List)
+	router.Get("/{id}", orderHandler.GetByID)
+	router.Put("/{id}", orderHandler.UpdateByID)
+	router.Delete("/{id}", orderHandler.DeleteByID)
 }
--- main.go
+++ main.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import (
+	"context"
+	"fmt"
+	"restmanage/application"
+)
 
+func main() {
+	app := application.New()
+
+	err := app.Start(context.TODO())
+	if err != nil {
+		fmt.Println("failed to start app:", err)
+	}
 }
