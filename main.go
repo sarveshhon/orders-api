22314b3359d5b5d8e9e1e0e8161a1af7d6b33ec2
Sarvesh Hon <sarveshhon@gmail.com>
2024-02-26 00:09:24 +0530

    Graceful shutdown complete

--- main.go
+++ main.go
@@ -3,14 +3,20 @@ package main
 import (
 	"context"
 	"fmt"
+	"os"
+	"os/signal"
 	"restmanage/application"
 )
 
 func main() {
 	app := application.New()
 
-	err := app.Start(context.TODO())
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer cancel()
+
+	err := app.Start(ctx)
 	if err != nil {
 		fmt.Println("failed to start app:", err)
 	}
+
 }
